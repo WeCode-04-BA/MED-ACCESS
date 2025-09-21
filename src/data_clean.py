cb2b53646c6495ac0af0f3ab801f1b67a8bc8ae5
Washu Ravele <washuravele75@gmail.com>
2025-09-21 11:23:09 +0200

    updated data_clean.py

--- src/data_clean.py
+++ src/data_clean.py
@@ -1,46 +1,49 @@
 import pandas as pd
 
 #import the disease and symptoms dataset
-diseases = pd.read_csv("./data/raw/dataset.csv")
+dataset = pd.read_csv("./data/raw/Disease and symptoms dataset.csv")
 
-#strip spaces,lowercase,remove duplicates,drop empty symptoms columns
-for col in diseases.columns:
-    diseases[col] = diseases[col].astype(str).str.strip().str.lower().str.replace(r"[\s_]+","_",regex=True)
-    diseases[col] = diseases[col].replace("nan",pd.NA)
+#drop duplicates
+dataset = dataset.drop_duplicates()
+ 
+#import the WHO dataset
+who_dataset = pd.read_csv("./data/raw/GHE_FULL_DD.csv")
 
-diseases = diseases.drop_duplicates()   
+# get South Africa data from WHO dataset
 
-#drop columns with 80% missing values
-threshold = len(diseases) * 0.2
-diseases = diseases.dropna(axis=1,thresh=threshold)
+sa_data = who_dataset[who_dataset['DIM_COUNTRY_CODE'] == 'ZAF']
+
+#lower case  and remove spaces to allow them to combine without any problem
+sa_disease = sa_data["DIM_GHECAUSE_TITLE"].str.lower().str.strip()
+dataset["Disease_clean"] = dataset["diseases"].str.lower().str.strip()
+
+# find matching disease
+matching_diseases = dataset[dataset["Disease_clean"].isin(sa_disease)]
+
+# Adding new diseases to matching list
+additions = ["human immunodeficiency virus infection(hiv)",
+             "lung cancer","brain cancer","allergy","panic attack",
+             "diabetes","heart attack","tooth disorder","skin cancer",
+             "pregnancy","depression","drug abuse","food allergy","alcoholic liver disease",
+             "flu","chickenpox"]
+
+# disease names from matched list
+matched_disease_names = matching_diseases["Disease_clean"].unique().tolist()
+
+#combine addition and matched lists
+final_diseases_name = list(set(matched_disease_names + additions))
 
 
-#export  the  disease and symptoms dataset
-diseases.to_csv("./data/processed/cleaned_dataset.csv",index=False)
-print("disease and symptoms dataset successfully exported")
+# filter full dataset
+final_filtered_dataset  = dataset[dataset["Disease_clean"].isin(final_diseases_name)]
 
+#remove the disease_clean column
+final_filtered_dataset.drop(columns=["Disease_clean"] , inplace = True)
 
-#import the description,precaution, and severity datasets
-description = pd.read_csv("./data/raw/symptom_Description.csv")
-precaution = pd.read_csv("./data/raw/symptom_precaution.csv")
-severity = pd.read_csv("./data/raw/Symptom-severity.csv")
+# done cleaned
+dataset = final_filtered_dataset
+print(dataset.head())
 
-#low-case description(disease and description column)
-description["Disease"] = description["Disease"].str.lower()
-description["Description"] = description["Description"].str.lower()
-
-#export the description dataset
-description.to_csv("./data/processed/cleaned_symptoms_description.csv",index=False)
-print("disease and description dataset successfully exported")
-
-#low case precaution dataset
-for  col in  precaution.columns:
-   precaution[col] =  precaution[col].str.lower()
-
-#export the precaution dataset
-precaution.to_csv("./data/processed/cleaned_symptom_precaution.csv",index = False)
-print("disease and precautions dataset successfully exported")
-
-# severity dataset is clean lets export it
-severity.to_csv("./data/processed/cleaned_symptom_severity.csv",index=False)
-print("symptom and weight dataset successfully exported")
+#export the dataset
+dataset.to_csv("./data/processed/clean_dataset.csv",index=False)
+print("Succefully exported")
