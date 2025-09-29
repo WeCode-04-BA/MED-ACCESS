3b86c942d0d3ebb0374fb0eeed9cd77026392220
Washu Ravele <washuravele75@gmail.com>
2025-09-29 20:02:54 +0200

    modified train model python file

--- src/train_model.py
+++ src/train_model.py
@@ -5,8 +5,18 @@ import pandas as pd
 # Models from Scikit-Learn
 from sklearn.preprocessing import LabelEncoder
 from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import LinearSVC
+from sklearn.neighbors import KNeighborsClassifier
+
 #Model Evaluations
-from sklearn.model_selection import train_test_split,cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import  accuracy_score
+
 
 #import dataset
 dataset = pd.read_csv("./data/processed/clean_dataset.csv")
@@ -25,24 +35,42 @@ X_train , X_test , y_train , y_test = train_test_split(X,y,test_size=0.2)
 #Set random seed
 np.random.seed(42)
 
-#create a new classifier with best parameters
-clf = GaussianNB(var_smoothing = 1e-09)
+# Creating a dictionary
+models = {"Decision Tree" :DecisionTreeClassifier(),
+          "Naïve Bayes" : GaussianNB(),
+          "Logistic Regression": LogisticRegression(),
+          "Random Forest": RandomForestClassifier(),
+          "LinearSVC": LinearSVC(),
+          "KNeighbors": KNeighborsClassifier()}  
 
-#fit the model
-clf.fit(X_train,y_train)
 
-#accuracy score
-print(f"accuracy score: {clf.score(X_test,y_test)}")
+# Train and evaluate
+best_model = None
+best_score = 0
+model_scores = {}
 
-#predicted values
-print(f"predicted values: {clf.predict(X_test)}")
+for name, model in models.items():
+    
+     #Set random seed
+    np.random.seed(42)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    score = accuracy_score(y_test, y_pred)
+    model_scores[name] = score
+    print(f"{name} Accuracy: {score:.2f}")
+    
+    if score > best_score:
+        best_score = score
+        best_model = model
 
-#decode predicted values
-decode_preds = le.inverse_transform(clf.predict(X_test))
-print(f"decode  predicted values:{decode_preds}")
 
 # Export the model
-from joblib import dump , load
+from joblib import dump 
+
 # save model to file
-dump(clf,filename = "./Models/best_model.joblib")
+dump(best_model,filename = "./Models/best_model.joblib")
 print("Successfully exported the model")
+print(f"\n Best model saved: {type(best_model).__name__} with accuracy {best_score:.2f}")
+
+
+
